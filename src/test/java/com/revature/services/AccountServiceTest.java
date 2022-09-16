5d0d86551c507465841dea9cf6920d273ab8b914
Kira Smiley <kira640@revature.net>
2022-09-16 17:04:37 -0400

    completed UserService testing

---
+++ src/test/java/com/revature/services/AccountServiceTest.java
@@ -0,0 +1,89 @@
+package com.revature.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccountServiceTest {
+
+	@Test
+	void testFindByUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpsertAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllTransactions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpsertTransaction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
