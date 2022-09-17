a37a7fdc1c5f3ea5312d98f8b134783be3bfeb82
Kira Smiley <kira640@revature.net>
2022-09-16 21:28:48 -0400

    completed AuthService testing

--- src/test/java/com/revature/services/AccountServiceTest.java
+++ src/test/java/com/revature/services/AccountServiceTest.java
@@ -1,14 +1,48 @@
 package com.revature.services;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.revature.models.Account;
+import com.revature.models.User;
+import com.revature.repositories.AccountRepository;
+import com.revature.repositories.TransactionRepository;
+import com.revature.repositories.UserRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+import java.time.Instant;
+import java.time.ZoneOffset;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 class AccountServiceTest {
 
+	private Instant instant = Instant.now();
+	private User testUser = new User(1, "example@example.com", "password");
+	private Account testAccount = new Account("Primary Checking	", "A really nice bank account to track my checking account transactions", 
+			instant, testUser);
+	@InjectMocks
+	private AccountService accountService = Mockito.mock(AccountService.class);
+	private static AccountRepository mockAcctRepo = Mockito.mock(AccountRepository.class);
+	private static TransactionRepository mockTransRepo = Mockito.mock(TransactionRepository.class);
+	private static UserRepository mockRepo = Mockito.mock(UserRepository.class);
+	@Mock
+	private static UserService userService = Mockito.mock(UserService.class);
+	
+	@BeforeAll
+	public static void userService() {
+		userService.findById(1);
+	}
+	
 	@Test
 	void testFindByUserId() {
-		fail("Not yet implemented");
+		Mockito.when(userService.findById(1)).thenReturn(testUser);
+		Optional<Account> testOptional = mockAcctRepo.findByUser(testUser);
+		assertEquals(testOptional, testAccount);
 	}
 
 	@Test
@@ -26,64 +60,5 @@ class AccountServiceTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	void testObject() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetClass() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testHashCode() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testEquals() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testClone() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testToString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testNotify() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testNotifyAll() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testWait() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testWaitLong() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testWaitLongInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testFinalize() {
-		fail("Not yet implemented");
-	}
 
 }
