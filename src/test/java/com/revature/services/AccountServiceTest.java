43b5961709fcc053dc425976d71d2208ea44e1d8
smileykira237 <73151121+smileykira237@users.noreply.github.com>
2022-09-20 15:46:11 -0400

    Password reset (#26)
    * added abilty to send recovery emails with tokens
    
    * added abbility to retreve tokens from database
    
    * fixed error when inputing wrong email
    
    * added reset putmapping
    
    * added put method to update password
    
    * added throws exception
    
    * added logging
    
    * completed UserService testing
    
    * completed AuthService testing
    
    * completed service layer testing
    
    * added testing
    
    * added testing
    
    * made tokens single use
    
    * changed test email
    
    * revert email change
    
    also added comments
    
    * missed the gmail
    
    Co-authored-by: delay970 <foxmatt26@gmail.com>
    Co-authored-by: delay970 <60533032+delay970@users.noreply.github.com>
    Co-authored-by: Kira Smiley <kira640@revature.net>
    Co-authored-by: Matthew Nelson <matthewnelson253@gmail.com>

---
+++ src/test/java/com/revature/services/AccountServiceTest.java
@@ -0,0 +1,65 @@
+package com.revature.services;
+
+import com.revature.models.Account;
+import com.revature.models.Transaction;
+import com.revature.models.User;
+import com.revature.repositories.AccountRepository;
+import com.revature.repositories.TransactionRepository;
+import com.revature.repositories.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+class AccountServiceTest {
+
+	private User testUser = new User(1, "example@example.com", "password");
+	private Optional<Account> testAccount = Optional.of(new Account(1, "Primary Checking", 10000.00,"A really nice bank account to track my checking account transactions", Instant.now(), null));
+	List<Transaction> testTransaction = new ArrayList<Transaction>(0);
+	@Mock
+	private AccountRepository accountRepository;
+	@Mock
+	private TransactionRepository transRepo;
+	@Mock
+	private UserService userService;
+	@InjectMocks
+	private AccountService as;
+	
+	@BeforeEach
+	public void userService() {
+		MockitoAnnotations.openMocks(this);
+	}
+	
+	@Test
+	void testFindByUserId() {
+		
+		Mockito.when(userService.findById(1)).thenReturn(testUser);
+		Mockito.when(accountRepository.findByUser(Mockito.any(User.class))).thenReturn(testAccount);
+		Optional<Account> serviceTest = as.findByUserId(1);
+
+		
+		assertEquals(serviceTest, testAccount);
+	}
+
+	@Test
+	void testGetAllTransactions() {
+		Account testAccount = new Account(0, null, 0.0,null, null, null);
+		Mockito.when(accountRepository.getById(Mockito.anyInt())).thenReturn(testAccount);
+		Mockito.when(transRepo.findByAccount(Mockito.any(Account.class))).thenReturn(testTransaction);
+		List<Transaction> serviceTest = as.getAllTransactions(1);
+
+		
+		assertEquals(serviceTest, testTransaction);
+	}
+
+}
