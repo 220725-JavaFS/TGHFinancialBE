a23119e23821a835d0b0860d40fe27c7292ac192
NigelBarnes <110052724+NigelBarnes@users.noreply.github.com>
2022-09-20 16:26:40 -0400

    Reapplied code that was removed

--- src/main/java/com/revature/models/Account.java
+++ src/main/java/com/revature/models/Account.java
@@ -26,7 +26,7 @@ public class Account {
     String description;
     Instant creationDate;
 
-    @OneToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(referencedColumnName = "id")
     @JsonIgnore
     User user;
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,10 +1,13 @@
 package com.revature.services;
 
-import com.revature.models.User;
-import com.revature.repositories.UserRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import com.revature.models.Account;
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
 
 @Service
 public class UserService {
