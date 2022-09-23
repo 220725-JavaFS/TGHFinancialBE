8a1d8571a19bb6ff11016a613e1ab3daf3a683c4
Kira Smiley <kira640@revature.net>
2022-09-23 16:55:00 -0400

    made emails unique

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -16,6 +16,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(unique = true)
     private String email;
     private String password;
     private boolean darkmode;
