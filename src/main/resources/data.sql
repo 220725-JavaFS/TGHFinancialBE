6433a99307c26a606608e14cd7d47fd980924f66
BlizWoRo <saumik148@revature.net>
2022-09-26 12:10:44 -0500

    fixed sendMoney for frontend validation got too high amount transfer

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,6 +20,14 @@ insert into accounts (id, name, balance, description, creation_date, user_id) va
     '2022-08-25T20:32:26.568Z',
     1
 );
+insert into accounts (id, name, balance, description, creation_date, user_id) values (
+    2,
+    'Secondary Checking',
+    10000.00,
+    'A really nice bank account to track my second checking account transactions',
+    '2022-08-25T20:32:26.568Z',
+    1
+);
 
 insert into transactions (id, amount, description, type, account_id) values (
     1,
